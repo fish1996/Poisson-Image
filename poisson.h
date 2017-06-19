03988520cb48d982b9fe92c82e23993bd299a787
fish1996 <370299459@qq.com>
2017-06-19 13:32:07 +0800

    添加set注释

--- poisson.h
+++ poisson.h
@@ -1,4 +1,4 @@
-#ifndef POISSON_H
+﻿#ifndef POISSON_H
 #define POISSON_H
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
@@ -83,11 +83,17 @@ private:
 
 public:
     Poisson();//src是原图，add和mask都是处理后得到的
+    //颜色改变，含掩码
     void set(Mat* src, Mat* mask, Color c, int times, int x, int y, int w, int h);
+    //颜色改变，不含掩码
     void set(Mat* src, Color c, int times, int x, int y, int w, int h);
+    //光照，含掩码
     void set(Mat* src, Mat* mask, float alpha, float beta, int times, int x, int y, int w, int h);
+    //光照，不含掩码
     void set(Mat* src, float a, float b, int times, int x, int y, int w, int h);
+    //无缝克隆，含掩码，如果是优化的无缝克隆传入后面三个参数
     void set(Mat* src, Mat* add, Mat* mask, int times, int x, int y, int w, int h, float min = 0, float max = 1, int factor = 10);
+    //无缝克隆，不含掩码，如果是优化的无缝克隆传入后面三个参数
     void set(Mat* src, Mat* add, int times, int x, int y, int w, int h, float min = 0, float max = 1, int factor = 10);
     void set(Mat* src, Mat* mask, float lt, float ht, int size, int times, int x, int y, int w, int h);
 
